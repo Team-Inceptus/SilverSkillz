31d74bc40f3e831b83b35a05d93a22dfc6c71b23
GamerCoder215 <gamercoder215@gmail.com>
2021-10-29 22:11:20 -0500

    Update SilverSkillz.java
    - Added Events

--- src/me/gamercoder215/silverskillz/SilverSkillz.java
+++ src/me/gamercoder215/silverskillz/SilverSkillz.java
@@ -2,12 +2,18 @@ package me.gamercoder215.silverskillz;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.gamercoder215.silverskillz.skills.SkillAdvancer;
+import me.gamercoder215.silverskillz.skills.SkillUtils;
+
 public class SilverSkillz extends JavaPlugin {
 
 	public void onEnable() {
 		this.saveDefaultConfig();
 		this.saveConfig();
-
+		
+		new SkillAdvancer(this);
+		new SkillUtils(this);
+		
 		this.saveConfig();
 	}
 
