2ec3c3d998f9e4a5324586b51360f6169b252e97
GamerCoder215 <gamercoder215@gmail.com>
2022-01-17 01:14:25 -0600

    Update Abilities.java
    Executor for Premium Commands

--- src/us/teaminceptus/silverskillz/premium/commands/Abilities.java
+++ src/us/teaminceptus/silverskillz/premium/commands/Abilities.java
@@ -1,5 +1,17 @@
 package us.teaminceptus.silverskillz.premium.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import us.teaminceptus.silverskillz.SilverPlayer;
+import us.teaminceptus.silverskillz.SilverSkillz;
+import us.teaminceptus.silverskillz.premium.skills.Ability;
+import us.teaminceptus.silverskillz.skills.SkillUtils;
+
 class Abilities implements CommandExecutor {
 
 	protected SilverSkillz plugin;
@@ -13,7 +25,12 @@ class Abilities implements CommandExecutor {
 		if (!(sender instanceof Player p)) return false;
 
 		SilverPlayer sp = new SilverPlayer(p);
-			
+		
+		Inventory gui = SkillUtils.generateGUI(45, ChatColor.DARK_AQUA + "Player Abilities");
+		
+		for (Ability a : Ability.values()) gui.addItem(a.isUnlocked(sp) ? a.generateItemStack() : a.generateLockedItem());
+		
+		p.openInventory(gui);
 		return true;
 	}
 	
